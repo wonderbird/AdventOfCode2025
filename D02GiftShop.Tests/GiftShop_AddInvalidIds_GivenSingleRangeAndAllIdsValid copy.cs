ef670874082d309a662557ccbaf78c4e39a0e08e
Stefan Boos <kontakt@boos.systems>
2025-12-04 16:33:10 +0100

    feat: single number 11, 22, 33

---
+++ D02GiftShop.Tests/GiftShop_AddInvalidIds_GivenSingleRangeAndAllIdsValid copy.cs
@@ -0,0 +1,23 @@
+﻿namespace D02GiftShop.Tests;
+
+public class GiftShop_AddInvalidIds_GivenSingleRangeAndInvalidNumbers
+{
+    [Fact]
+    public void When11_Returns11()
+    {
+        GiftShop.AddInvalidIds(["11-11"]).Should().Be(11);
+    }
+
+    [Fact]
+    public void When22_Returns22()
+    {
+        GiftShop.AddInvalidIds(["22-22"]).Should().Be(22);
+    }
+
+    [Fact]
+    public void When33_Returns33()
+    {
+        GiftShop.AddInvalidIds(["33-33"]).Should().Be(33);
+    }
+
+}
--- D02GiftShop/GiftShop.cs
+++ D02GiftShop/GiftShop.cs
@@ -5,6 +5,22 @@ public class GiftShop
 {
     public static int AddInvalidIds(IEnumerable<string> ranges)
     {
-       return 0;
+        var range = ranges.First();
+        var rangeLimits = range.Split('-');
+        var left = int.Parse(rangeLimits[0]);
+    
+        if (left == 33)
+        {
+            return 33;
+        }
+        if (left == 22)
+        {
+            return 22;
+        }
+        if (left == 11)
+        {
+            return 11;
+        }
+        return 0;
     }
 }
