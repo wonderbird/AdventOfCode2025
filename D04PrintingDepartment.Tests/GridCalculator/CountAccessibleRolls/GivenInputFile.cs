615d57fb0f87c6c11b2532a1562bc4e5588872dc
Stefan Boos <kontakt@boos.systems>
2025-12-08 05:49:21 +0100

    test: when single row accessible

---
+++ D04PrintingDepartment.Tests/GridCalculator/CountAccessibleRolls/GivenInputFile.cs
@@ -0,0 +1,15 @@
+﻿namespace D04PrintingDepartment.Tests.GridCalculator.CountAccessibleRolls;
+
+public class GivenInputFile
+{
+    [Theory]
+    [InlineData("ExampleInput.txt", 13)]
+    [InlineData("PuzzleInput.txt", 1389)]
+    [InlineData("OnlyOneRemovalStepPossible.txt", 1)]
+    public void WhenFileName_ReturnsExpected(string fileName, int expected)
+    {
+        var path = Path.Combine("Data", fileName);
+        var input = File.ReadAllLines(path);
+        new D04PrintingDepartment.GridCalculator(input).AccessibleRolls.Should().Be(expected);
+    }
+}
---
+++ D04PrintingDepartment.Tests/GridCalculator/CountAccessibleRolls/GivenSingleRow.cs
@@ -0,0 +1,16 @@
+﻿namespace D04PrintingDepartment.Tests.GridCalculator.CountAccessibleRolls;
+
+public class GivenSingleRow
+{
+    [Fact]
+    public void Returns0()
+    {
+        new D04PrintingDepartment.GridCalculator(["."]).AccessibleRolls.Should().Be(0);
+    }
+
+    [Fact]
+    public void GivenSingleRoll_Returs1()
+    {
+        new D04PrintingDepartment.GridCalculator(["@"]).AccessibleRolls.Should().Be(1);
+    }
+}
